01dbd8c9ba18e454878706919beb01f31a9e8ba8
Kunal <kunal123452003@gmail.com>
2026-04-21 21:10:38 +0530

    Done with RNN

--- Deep Learning for NLP/Simple_RNN/main.py
+++ Deep Learning for NLP/Simple_RNN/main.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence 
+# from tensorflow.keras.models import load_model
 from tensorflow.keras.models import load_model
 import streamlit as st
 
@@ -12,7 +13,7 @@ word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
 
 # 3) Load the saved model
-model = load_model('SimpleRNN_imdb.h5')
+model = load_model('SimpleRNN_imdb_improved.keras')
 
 # Step 2: helper func
 def decode_review(encoded_review):
@@ -20,6 +21,7 @@ def decode_review(encoded_review):
 
 # func to process text
 def preprocess_text(text):
+    print("Original review:", text)
     words = text.lower().split()
     encoded_review = [word_index.get(word, 2) +3 for word in words]
     padded_review =sequence.pad_sequences([encoded_review], maxlen=500)
@@ -28,8 +30,8 @@ def preprocess_text(text):
 # Step 3 : prediction function
 ## prediction fucntion
 def predict_sentiment(text_review):
-    preprocessed_input = preprocess_text(text_review)
-    prediction = model.predict(preprocessed_input)
+    # preprocessed_input = preprocess_text(text_review)
+    prediction = model.predict(text_review)
     sentiment = "Positive" if prediction[0][0] > 0.5 else "Negative"
 
     return sentiment, prediction[0][0]
@@ -39,14 +41,17 @@ st.title("IMDB movie review sentiment analysis")
 st.write("Enter a movie review to predict it as positive or negative.")
 
 # User input
-user_input = st.text_area("movie review", height=150)
+user_input = st.text_area("movie review")
+print("User input:", user_input)
 
 if st.button("Sentiment"):
 
     preprocess_input = preprocess_text(user_input)
+    st.write("Preprocessed input:", preprocess_input)
 
     sentiment, prediction = predict_sentiment(preprocess_input)
-    st.write(f"Predicted Sentiment: {sentiment} Prediction Score: {prediction} ")
+    st.write(f"Predicted Sentiment: {sentiment}")
+    st.write(f"Prediction Score: {prediction} ")
 
 else:
     st.write("Please enter a movie review and click the button to predict its sentiment.")
